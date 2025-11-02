f22d715c5d94e4f52ef84554a92157f9179c4e0d
ValoriaCraft770 <bilalbabapro4124@gmail.com>
2025-11-02 13:37:14 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+const mineflayer = require('mineflayer');
+
+// Sunucu bilgilerini BURAYA yaz
+const bot = mineflayer.createBot({
+  host: 'ValoriaCraft.aternos.me', // örnek: valoriacraft.aternos.me
+  port: 29946, // değişmediyse bu kalsın
+  username: 'BotIsminiz', // Premium hesapsa e-posta / Cracked sunucuysa sadece isim
+  version: false // Sunucu sürümünü otomatik bulur
+});
+
+// Konsol logları
+bot.on('login', () => console.log(`[BOT] Giriş yaptı: ${bot.username}`));
+bot.on('spawn', () => console.log('[BOT] Sunucuda spawn oldu.'));
+bot.on('end', () => console.log('[BOT] Bağlantı koptu, yeniden bağlanacak...'));
+
+// Bağlantı koparsa 10 saniye sonra yeniden bağlan
+bot.on('end', () => setTimeout(() => process.exit(0), 10000));
+bot.on('error', err => console.log(`[Hata] ${err}`));
+
+// Küçük hareket (AFK tespiti engeller)
+setInterval(() => {
+  const yaw = Math.random() * Math.PI * 2;
+  bot.look(yaw, 0, false);
+}, 60000);
